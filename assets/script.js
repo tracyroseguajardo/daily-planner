40fa95879bef96840185f853295d8a0f01eab972
tracy rose guajardo <tracyroseguajardo@gmial.com>
2022-05-30 10:45:51 -0700

    structure in place

---
+++ assets/script.js
@@ -0,0 +1,39 @@
+//GLOBAL VARIABLES
+// today's date via momentjs
+var today = moment();
+$("#currentDay").text(today.format("MMM Do, YYYY"));
+
+//current time via momentjs
+// console.log(presentHour);
+
+function format() {
+    var presentHour = moment().hours();
+
+    $('.time-block').each(function(){
+        var hour = parseInt($(this).attr('id'));
+
+        if (hour < presentHour){
+            $(this).addClass('past')
+        }
+    })
+
+}
+format() 
+$('.saveBtn').on('click', function(){
+
+        // the parent div id
+        var key = $(this).parent().attr('id');
+        // value will be the textarea content
+        var value = $(this).siblings('.description').val();
+
+    localStorage.setItem(key, value)
+});
+
+$('#9 .description').val(localStorage.getItem('9'))
+$('#10 .description').val(localStorage.getItem('10'))
+
+
+
+
+
+

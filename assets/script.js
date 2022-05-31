a4d872d347a5c8dc7117b409482409eff4379c50
tracy rose guajardo <tracyroseguajardo@gmial.com>
2022-05-31 15:23:54 -0700

    sets and gets local storage

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,16 @@
 //GLOBAL VARIABLES
+
 // today's date via momentjs
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
-//current time via momentjs
-// console.log(presentHour);
-
 //DECLARED FUNCTIONS
 
 function format() {
+    //current time via momentjs
     var presentHour = moment().hours();
 
+    //adds present, past, future classes
     $('.time-block').each(function(){
         var hour = parseInt($(this).attr('id'));
 
@@ -26,24 +26,28 @@ function format() {
             $(this).addClass('future');
         }
     })
-
 }
-format() 
+
+format()
+
+//sets local storage to save tasks
 $('.saveBtn').on('click', function(){
 
         // the parent div id
         var key = $(this).parent().attr('id');
-        // value will be the textarea content
+        // value will be the text area content
         var value = $(this).siblings('.description').val();
 
     localStorage.setItem(key, value)
 });
 
+//allows saved tasks to remain displayed, even if window closed
 $('#9 .description').val(localStorage.getItem('9'))
 $('#10 .description').val(localStorage.getItem('10'))
-
-
-
-
-
-
+$('#11 .description').val(localStorage.getItem('11'))
+$('#12 .description').val(localStorage.getItem('12'))
+$('#13 .description').val(localStorage.getItem('13'))
+$('#14 .description').val(localStorage.getItem('14'))
+$('#15 .description').val(localStorage.getItem('15'))
+$('#16 .description').val(localStorage.getItem('16'))
+$('#17 .description').val(localStorage.getItem('17'))
